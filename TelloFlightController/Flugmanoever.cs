ed12391d152531487bdd8cd09885011d4bf10722
AlexUlz <117368695+AlexUlz@users.noreply.github.com>
2023-06-27 15:51:33 +0200

    All Files added

---
+++ TelloFlightController/Flugmanoever.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelloFlightController
+{
+    /*
+   @Luck007, zylex, xela014
+   @1.0
+   Interface für die Klasse Mehreck
+   */
+    internal interface Flugmanoever
+    {
+        void Check_isFlying();
+        void Fly();
+    }
+}
